f0388ae35b99d601fd917b78eabf02d7b7a7390b
LeNgocHai1 <81201425+LeNgocHai1@users.noreply.github.com>
2021-11-02 23:59:24 +0700

    Update Users.php

--- app/Users.php
+++ app/Users.php
@@ -9,6 +9,5 @@ class Users extends Model
     protected $table = 'users';
     protected $primaryKey = 'id';
 
-    protected $fillable = ['name','email','password','sdt','address','role'];
     protected $fillable = ['name','email','password','phonenumber','address','note','role'];
 }
