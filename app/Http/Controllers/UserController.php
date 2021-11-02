f6658061c1c2e347ed60652f15afaef3268e574e
LeNgocHai <19211TT1307@mail.tdc.edu.vn>
2021-11-02 23:21:16 +0700

    update to Hai

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Users;
+use Illuminate\Support\Facades\DB;
+use App\Bill;
+use App\BillDetail;
+use App\Customer;
+use Illuminate\Support\Facades\Session;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('detailuser');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = Users::find($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->phonenumber = $request->phonenumber;
+        $user->address = $request->address;
+
+        Session::put('message','Sửa thông tin tài khoản thành công');
+        
+        $user->save();
+        return view('detailuser');
+    }
+
+
+}
