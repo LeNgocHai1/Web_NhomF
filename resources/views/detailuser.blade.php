b825d2268ccbc4ab9a4bafb03641f722ea7fc408
viethoang1510 <90826691+viethoang1510@users.noreply.github.com>
2021-11-04 21:10:08 +0700

    Update detailuser.blade.php

--- resources/views/detailuser.blade.php
+++ resources/views/detailuser.blade.php
@@ -1,54 +1,58 @@
 
 @extends('pages.customer.main')
 @section('content')
-    <div class="container-fluid jumbotron">
-        <div class="row">
-    </div>
-    <div class="  bg-white border rounded border-info col-sm-10">
-        <h3 class="mt-3" style="font-weight: bold; color: #4A235A;">Thông tin tài khoản</h3>
-        <?php
+<div class="row">
+    <div class="col-md-6">
+        <div class="  bg-white border rounded border-info col-sm-10">
+            <h3 class="mt-3" style="font-weight: bold; color: #4A235A;">Thông tin tài khoản</h3>
+            <?php
                                 $message = Session::get('message');
                                 if($message){
                                     echo '<span class="text-alert">'.$message.'</span>';
                                     Session::put('message',null);
                                 }
                                 ?>
-        <div class="row">
-        
-            <div class="col-sm-10 mt-3">
-                <form action="{{ route('info.update',Auth::user()->id) }}" method="post" enctype="multipart/form-data">
-                {{ csrf_field() }}
-                @method('PUT')
-            
-                <div class="form-group">
-                    <label for="name" style="font-weight: bold">Tên người dùng</label>
-                    <input type="text" name="name" id="name" class="form-control" value="{{ Auth::user()->name }}">
-                </div>
-                <div class="form-group">
-                    <label for="email" style="font-weight: bold">Email</label>
-                    <input type="text" name="email" id="email" class="form-control" value="{{ Auth::user()->email }}">
-                
-                </div>
+            <div class="row">
 
-                <div class="form-group">
-                    <label for="phonenumber" style="font-weight: bold">Số điện thoại</label>
-                    <input type="text" name="phonenumber" id="address" class="form-control" value="{{ Auth::user()->phonenumber }}">
-                </div>
-                <div class="form-group">
-                    <label for="address" style="font-weight: bold">Địa chỉ</label>
-                    <input type="text" name="address" id="address" class="form-control" value="{{ Auth::user()->address }}">
-                </div>
-            
-        
-                <div class="form-group">
-                    <input type="submit" class="btn btn-info btn-sm" value="Cập Nhật">
+                <div class="col-sm-10 mt-3">
+                    <form action="{{ route('info.update',Auth::user()->id) }}" method="post"
+                        enctype="multipart/form-data">
+                        {{ csrf_field() }}
+                        @method('PUT')
 
-                </div>
+                        <div class="form-group">
+                            <label for="name" style="font-weight: bold">Tên người dùng</label>
+                            <input type="text" name="name" id="name" class="form-control"
+                                value="{{ Auth::user()->name }}">
+                        </div>
+                        <div class="form-group">
+                            <label for="email" style="font-weight: bold">Email</label>
+                            <input type="text" name="email" id="email" class="form-control"
+                                value="{{ Auth::user()->email }}">
 
-                </form>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="phonenumber" style="font-weight: bold">Số điện thoại</label>
+                            <input type="text" name="phonenumber" id="address" class="form-control"
+                                value="{{ Auth::user()->phonenumber }}">
+                        </div>
+                        <div class="form-group">
+                            <label for="address" style="font-weight: bold">Địa chỉ</label>
+                            <input type="text" name="address" id="address" class="form-control"
+                                value="{{ Auth::user()->address }}">
+                        </div>
+
+
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-info btn-sm" value="Cập Nhật">
+
+                        </div>
+
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
     </div>
     <div class="col-md-6">
 
@@ -73,9 +77,9 @@
                                         style="border: none;font-weight: bold; color: #4A235A;">Mật Khẩu Cũ</label>
                                     <div class="col-md-12">
                                         <input id="oldpassword" type="password" name="oldpassword"
-                                            class="form-control-mn1"
-                                            name="oldpassword" required  style="width: 100%;">
-                                        <a class="btnPassword1" style="position: absolute;top:14%;right: 30px;color:#333;"><i
+                                            class="form-control-mn1" name="oldpassword" required style="width: 100%;">
+                                        <a class="btnPassword1"
+                                            style="position: absolute;top:14%;right: 30px;color:#333;"><i
                                                 class="fa fa-eye"></i></a>
                                         @if($errors->has('oldpassword'))
                                         <span class="invalid-feedback" role="alert"></span>
@@ -87,10 +91,10 @@
                                     <label class="col-md-4 control-label"
                                         style="border: none;font-weight: bold; color: #4A235A;">Mật Khẩu Mới</label>
                                     <div class="col-md-12">
-                                        <input id="password" type="password" name="password"
-                                            class="form-control-mn2"
-                                            required  style="width: 100%;">
-                                        <a class="btnPassword2" style="position: absolute;top:14%;right: 30px;color:#333;"><i
+                                        <input id="password" type="password" name="password" class="form-control-mn2"
+                                            required style="width: 100%;">
+                                        <a class="btnPassword2"
+                                            style="position: absolute;top:14%;right: 30px;color:#333;"><i
                                                 class="fa fa-eye"></i></a>
                                         @if($errors->has('password'))
                                         <span class="invalid-feedback" role="alert"></span>
@@ -105,7 +109,8 @@
                                     <div class="col-md-12">
                                         <input type="password" id="password_confirmation" name="password_confirmation"
                                             class="form-control-mn3" style="width: 100%;">
-                                        <a  class="btnPassword3" style="position: absolute;top:14%;right: 30px;color:#333;"><i
+                                        <a class="btnPassword3"
+                                            style="position: absolute;top:14%;right: 30px;color:#333;"><i
                                                 class="fa fa-eye"></i></a>
                                     </div>
                                 </div>
@@ -116,6 +121,14 @@
                     </div>
 
                 </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+
+@endsection
             </div>
         </div>
     </div>
